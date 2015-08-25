77abb5ced6abedd19d3899f505113e21b427711c
Nick Lalic <nick.lalic@gmail.com>
2015-08-24 22:14:57 -0700

    Still working on General.h => prettyAxis(..)

--- General/General.cpp
+++ General/General.cpp
@@ -817,33 +817,66 @@ void RsaToolbox::roundAxis(RowVector values, double interval, double &axis_min, 
     roundAxis(min(values), max(values), interval, axis_min, axis_max);
 }
 void RsaToolbox::prettyAxis(double &min, double &max, double &tickStep, int &subTickCount) {
-    tickStep = 1;
+//    qDebug() << "prettyAxis " << min << max;
+    tickStep = 1.0;
     subTickCount = 4;
+
+    double maxMantissa = abs(max);
+    double maxMagnitude = 1.0;
+    while (maxMantissa != 0.0 && maxMantissa < 1.0) {
+        maxMantissa *= 1.0E1;
+        maxMagnitude *= 1.0E-1;
+    }
+    while (maxMantissa >= 10.0) {
+        maxMantissa *= 1.0E-1;
+        maxMagnitude *= 1.0E1;
+    }
+    if (max < 0)
+        maxMantissa *= -1.0;
+
+    double minMantissa = abs(min);
+    double minMagnitude = 1.0;
+    while (minMantissa != 0.0 && minMantissa < 1.0) {
+        minMantissa *= 1.0E1;
+        minMagnitude *= 1.0E-1;
+    }
+    while (minMantissa >= 10.0) {
+        minMantissa *= 1.0E-1;
+        minMagnitude *= 1.0E1;
+    }
+    if (min < 0)
+        minMantissa *= -1.0;
+
+
+    if (minMagnitude > maxMagnitude) {
+        maxMantissa /= (minMagnitude/maxMagnitude);
+        maxMagnitude = minMagnitude;
+    }
+    else if (maxMagnitude > minMagnitude) {
+        minMantissa /= (maxMagnitude/minMagnitude);
+        minMagnitude = maxMagnitude;
+    }
+
+    roundAxis(minMantissa, maxMantissa, 5.0 / maxMagnitude, minMantissa, maxMantissa);
+    min = minMantissa * minMagnitude;
+    max = maxMantissa * maxMagnitude;
     double range = max - min;
-    if (range <= 0)
-        return;
-
-    double magnitude = 1;
-    while (range < 1) {
-        // Scale up
-        magnitude *= 1.0E-1;
-        range *= 1.0E1;
+    tickStep = int(range / 5);
+    if (int(tickStep) == 1)
+        subTickCount = 4;
+    else if (int(tickStep) == 2)
+        subTickCount = 3;
+    else if (int(tickStep) > 2 || int(tickStep) <= 5)
+        subTickCount = int(tickStep) - 1;
+    else {
+        // > 5
+        subTickCount = 4;
     }
-    while (range >= 10) {
-        // Scale down
-        magnitude *= 1.0E1;
-        range *= 1.0E-1;
-    }
-
-    double normalizedMin = min/magnitude;
-    double normalizedMax = max/magnitude;
-    roundAxis(normalizedMin, normalizedMax, 5.0, normalizedMin, normalizedMax);
-    if (normalizedMax - normalizedMin <= 5)
-        tickStep = 1.0*magnitude;
-    else
-        tickStep = 2.0*magnitude;
-    min = normalizedMin * magnitude;
-    max = normalizedMax * magnitude;
+//    qDebug() << "  returning...";
+//    qDebug() << "  max: " << max;
+//    qDebug() << "  min: " << min;
+//    qDebug() << "  tickStep: " << tickStep;
+//    qDebug() << "  subTickCount: " << subTickCount;
 }
 
 double RsaToolbox::linearInterpolateX(double x1, double y1, double x2, double y2, double y_desired) {
