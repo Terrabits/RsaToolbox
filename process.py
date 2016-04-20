2720c6bc6c484411d5d0f49f0ad13bd5f127cb51
Nick Lalic <nick.lalic@gmail.com>
2016-04-19 18:30:34 -0700

    Added build/ folder, python scripts to automate deployment

---
+++ process.py
@@ -0,0 +1,7 @@
+# Should do the following:
+# - Clean build byproducts?
+# - Regenerate documentation
+# - Process QtCreator
+# - Copy everything into /build directory
+# Should be left with _deploy folder with immaculate templates and instructions for install
+# Could create a "developer" mode for myself, with links to RsaToolbox results
